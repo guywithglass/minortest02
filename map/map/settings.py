b9dbe06779089c3a8e0686673c9f3b2f941e46a8
guywithglass <wirelessplaza987@gmail.com>
2024-04-19 02:58:35 +0530

    Setting Vercel Commit

--- map/map/settings.py
+++ map/map/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9^afhu2c4s-s)^!cf#kb7r!636f9o0#@z49d1r2ly0$k_&3)xp
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
