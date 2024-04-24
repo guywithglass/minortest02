ab1df1d876ca052909f9c2a8f02434dfc7fab240
guywithglass <wirelessplaza987@gmail.com>
2024-04-25 03:14:05 +0530

    Register, Login, and  Logout with messages

--- map/urls.py
+++ map/urls.py
@@ -26,7 +26,8 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('register/', views.register, name='register'),
-    path('login/', views.login, name='login')
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout')
 
 
 ]
--- map/views.py
+++ map/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import logout as auth_logout
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def index(request):
@@ -22,8 +26,44 @@ def about(request):
 
 
 def register(request):
+    if request.method == "POST":
+        username = request.POST['uname']
+        email = request.POST['email']
+        first_name = request.POST['fname']
+        password = request.POST['password']
+
+        if User.objects.filter(username=username).exists():
+            messages.error(request, 'Username is already taken.')
+            return redirect('register')
+        elif User.objects.filter(email=email).exists():
+            messages.error(request, 'Email is already in use.')
+            return redirect('register')
+        else:
+            user = User.objects.create_user(
+                first_name=first_name, email=email, username=username, password=password)
+            user.save()
+            print("User Created")
+            return redirect('index')
+
     return render(request, 'register.html')
 
 
 def login(request):
+    if request.method == 'POST':
+        username = request.POST['uname']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            auth_login(request, user)
+            messages.success(request, f'Welcome, {user.username}!')
+            print("User")
+            # return redirect('login')
+        else:
+            messages.error(request, 'Invalid login credentials.')
     return render(request, 'login.html')
+
+
+def logout(request):
+    auth_logout(request)
+    return redirect('index')
